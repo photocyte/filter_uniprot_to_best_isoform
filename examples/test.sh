01604212fee06ea01061275757ed11596293b587
Timothy Fallon <tfallon@mit.edu>
2018-05-21 19:18:01 -0400

    bug fixing

--- examples/test.sh
+++ examples/test.sh
@@ -29,6 +29,7 @@ wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/985/GCF_000002985.6_WBce
 echo "Now downloading the NCBI proteome FASTA file for Anopheles gambiae"
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/575/GCF_000005575.2_AgamP3/GCF_000005575.2_AgamP3_protein.faa.gz
 
+echo "Producing BLASTP database for testing Drosophila isoforms."
 zcat GCF_000002335.3_Tcas5.2_protein.faa.gz\
  GCF_000002195.4_Amel_4.5_protein.faa.gz\
  GCF_000151625.1_ASM15162v1_protein.faa.gz\
@@ -36,6 +37,7 @@ zcat GCF_000002335.3_Tcas5.2_protein.faa.gz\
  GCF_000005575.2_AgamP3_protein.faa.gz > for_drosophila.faa
 makeblastdb -dbtype prot -in for_drosophila.faa
 
+echo "Producing BLASTP database for testing Tribolium isoforms."
 zcat GCF_000001215.4_Release_6_plus_ISO1_MT_protein.faa.gz\
  GCF_000002195.4_Amel_4.5_protein.faa.gz\
  GCF_000151625.1_ASM15162v1_protein.faa.gz\
@@ -47,20 +49,20 @@ echo "Now running the filtering script on Drosophila, but exiting before the BLA
 python3 ../filter_uniprot_to_best_isoform.py -nb <(zcat 2018_05_uniprot-proteome_Dmelanogaster_GCA_000001215.4.fasta.gz)
 mv tmp.query.fa Drosophila.tmp.query.fa
 echo "Running blastp in parallel"
-/lab/solexa_weng/testtube/parallel_blast/parallel_blastp_map.sh Drosophila.tmp.query.fa for_drosophila.faa > Dmel_blastp.results.cache.tsv
+/lab/solexa_weng/testtube/parallel_blast/parallel_blastp_map.sh Drosophila.tmp.query.fa for_drosophila.faa Dmel_blastp.results.cache.tsv
 echo "Running the final filtering step... Find the results in isoform-filtered-*"
 python3 ../filter_uniprot_to_best_isoform.py -cr Dmel_blastp.results.cache.tsv -sb <(zcat 2018_05_uniprot-proteome_Dmelanogaster_GCA_000001215.4.fasta.gz) > isoform-filtered_2018_05_uniprot-proteome_Dmelanogaster_GCA_000001215.4.fasta
 echo "Done."
 echo ""
 
 echo "Now running the filtering script on Tribolium, but exiting before the BLAST step"
-python3 ../filter_uniprot_to_best_isoform.py -nb <(2018_05_uniprot-proteome_Tcastaneum_GCA_000002335.3.fasta.gz)
+python3 ../filter_uniprot_to_best_isoform.py -nb <(zcat 2018_05_uniprot-proteome_Tcastaneum_GCA_000002335.3.fasta.gz)
 mv tmp.query.fa Tribolium.tmp.query.fa
 echo "Running blastp in parallel"
-/lab/solexa_weng/testtube/parallel_blast/parallel_blastp_map.sh Tribolium.tmp.query.fa for_tribolium.faa > Tcas_blastp.results.cache.tsv
+/lab/solexa_weng/testtube/parallel_blast/parallel_blastp_map.sh Tribolium.tmp.query.fa for_tribolium.faa Tcas_blastp.results.cache.tsv
 echo "Running the final filtering step... Find the results in isoform-filtered-*"
 python3 ../filter_uniprot_to_best_isoform.py -cr Tcas_blastp.results.cache.tsv -sb <(zcat 2018_05_uniprot-proteome_Tcastaneum_GCA_000002335.3.fasta.gz) > isoform-filtered_2018_05_uniprot-proteome_Tcastaneum_GCA_000002335.3.fasta
 echo "Done."
 echo ""
 
-
+seqkit stat ./*.fasta ./*.fasta.gz
